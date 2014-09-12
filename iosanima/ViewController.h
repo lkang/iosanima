e1bed41a0bf818e8a02a6d9b83b782e2fd27fed0
lkang <lkang@sbcglobal.net>
2014-09-11 18:45:47 -0700

    Initial Commit

---
+++ iosanima/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iosanima
+//
+//  Created by Larry Kang on 9/11/14.
+//  Copyright (c) 2014 Larry Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iosanima/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iosanima
+//
+//  Created by Larry Kang on 9/11/14.
+//  Copyright (c) 2014 Larry Kang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
